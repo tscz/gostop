eb3fda05a549c0d90e92fc2783d38dd2b4d469ab
tscz <tscz@users.noreply.github.com>
2026-03-28 18:42:09 +0900

    preload svgs

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,25 @@
 import './i18n'
+import { CARD_IMAGE_URL } from './core/cards'
 import GameBoard from './components/GameBoard'
 
+const preloadStyle: React.CSSProperties = {
+  position: 'absolute',
+  width: 0,
+  height: 0,
+  overflow: 'hidden',
+  pointerEvents: 'none',
+}
+
 export default function App() {
-  return <GameBoard />
+  return (
+    <>
+      {/* Preload all card SVGs so they're cached before they appear in game */}
+      <div style={preloadStyle} aria-hidden="true">
+        {Object.values(CARD_IMAGE_URL).map(url => (
+          <img key={url} src={url} alt="" />
+        ))}
+      </div>
+      <GameBoard />
+    </>
+  )
 }
